bcbec51f8e89e34338e0765d9b9299934a83fc36
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-06-25 01:59:37 +0530

    creating website layout1

---
+++ Courses/php/ecommerce/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Online Shop</title>
+        <link rel="stylesheet" type="text/css" media="all" href="styles/style.css"/>
+    </head>
+    <body>
+        <div class="main_wrapper">
+
+            <div>This is Header</div>
+
+            <div>This is menubar</div>
+
+            <div>This is sidebar</div>
+
+            <div>This is content area</div>
+
+            <div>This is footer area</div>
+
+        </div>
+    </body>
+</html>
