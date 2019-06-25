eba0bac3fc0d530db4827d2958437983dc26f5ec
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-06-25 14:47:57 +0530

    Added static companies and brands

--- Courses/php/ecommerce/index.php
+++ Courses/php/ecommerce/index.php
@@ -31,7 +31,30 @@
 
       <div class="content_wrapper">
 
-        <div id="sidebar">This is sidebar</div>
+        <div id="sidebar">
+
+          <div id="sidebar_title">Categories</div>
+          <ul id="cats">
+            <li><a href="#">Laptops</a></li>
+            <li><a href="#">Computers</a></li>
+            <li><a href="#">Mobiles</a></li>
+            <li><a href="#">Cameras</a></li>
+            <li><a href="#">IPads</a></li>
+            <li><a href="#">Tablets</a></li>
+          </ul>
+
+          
+          <div id="sidebar_title">Brands</div>
+          <ul id="cats">
+            <li><a href="#">HP</a></li>
+            <li><a href="#">Dell</a></li>
+            <li><a href="#">Motorola</a></li>
+            <li><a href="#">Sony Ericson</a></li>
+            <li><a href="#">LG</a></li>
+            <li><a href="#">Apple</a></li>
+          </ul>
+
+        </div>
 
         <div id="content_area">This is content area</div>
 
