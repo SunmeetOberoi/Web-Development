0d3d9dca1da3aa038efe05f059b391e20be3e0c7
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-06-27 17:15:55 +0530

    Added search engine

--- Courses/php/ecommerce/functions/functions.php
+++ Courses/php/ecommerce/functions/functions.php
@@ -100,4 +100,38 @@ function get_all_products(){
     ";  
 	}   
 }
+
+function get_results(){
+  global $con;
+
+  if(isset($_GET['user_query'])){
+  
+    $search_query = $_GET['user_query'];
+    $get_pro_query = "SELECT * FROM products WHERE product_keyword LIKE '%$search_query%'"; 
+    $run_get_pro = mysqli_query($con, $get_pro_query);
+    if(mysqli_num_rows($run_get_pro)==0) {
+      echo "<h3 style='padding:20px';>No Products Found!</h3>";
+    }
+
+    while($row_pro = mysqli_fetch_array($run_get_pro)){
+      $product_id = $row_pro['product_id'];
+      $product_title = $row_pro['product_title'];
+      $product_price = $row_pro['product_price'];
+      $product_image = $row_pro['product_image'];
+
+      echo "
+        <div id='single_product'>
+          <h3>$product_title</h3>
+          <img src='admin_area/product_images/$product_image' width='180' height='180'/>
+
+          <p><b>$ $product_price</b></p>
+
+          <a href='details.php?pro_id=$product_id' style='float: left;'>Details</a>
+          <a href='index.php?pro_id=$product_id'><button style='float:right;'>Add to Cart</button></a>
+        </div>
+      
+      ";  
+    }
+  }   
+}
 ?>
