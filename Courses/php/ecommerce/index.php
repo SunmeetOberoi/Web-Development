3e9111669248cc30e14912de7229321fb8b7ff88
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-07-03 02:10:14 +0530

    styled login logout links

--- Courses/php/ecommerce/index.php
+++ Courses/php/ecommerce/index.php
@@ -58,9 +58,9 @@
 
               <?php
                 if(!isset($_SESSION['customer_email']))
-                  echo "<a href='checkout.php'>Login</a>";
+                  echo "<a href='checkout.php' style='color:orange;'>Login</a>";
                 else
-                  echo "<a href='logout.php'>Logout</a>";
+                  echo "<a href='logout.php' style='color:orange;'>Logout</a>";
               ?>
 
             </span>
