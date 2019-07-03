d7c117a247cacfe91f45b88202b8fa2b755a43d8
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-07-03 17:04:14 +0530

    Added redirect for sign up

--- Courses/php/ecommerce/details.php
+++ Courses/php/ecommerce/details.php
@@ -18,11 +18,11 @@
 
       <div class="menubar">
         <ul id="menu">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">All Products</a></li>
-          <li><a href="#">My Account</a></li>
-          <li><a href="#">Sign Up</a></li>
-          <li><a href="#">Shopping Cart</a></li>
+        <li><a href="index.php">Home</a></li>
+          <li><a href="all_products.php">All Products</a></li>
+          <li><a href="customer/my_account.php">My Account</a></li>
+          <li><a href="customer_register.php">Sign Up</a></li>
+          <li><a href="cart.php">Shopping Cart</a></li>
           <li><a href="#">Contact Us</a></li>
         </ul>
         <div id="form">
