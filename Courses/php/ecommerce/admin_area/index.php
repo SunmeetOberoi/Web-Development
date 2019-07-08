74bc2cb17e9f966d61359d5c4ee0a472c7eac0bb
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-07-08 10:21:10 +0530

    Created the admin panel layout

---
+++ Courses/php/ecommerce/admin_area/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <title>This is Admin Panel</title>
+
+    <link rel="stylesheet" type="text/css" href="styles/style.css" media="all" >
+  </head>
+
+  <body>
+    <div class="main_wrapper">
+
+      <div id="header"></div>
+      <div id="right">Manage Content</div>
+      <div id="left"></div>
+    </div>
+  </body>
+
+</html>
