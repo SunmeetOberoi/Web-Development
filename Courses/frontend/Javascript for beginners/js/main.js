e2cbd51065e2726c5c8b4ff6bbaa2ee7297b419f
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-07-26 14:40:49 +0530

    innerHTML

--- Courses/frontend/Javascript for beginners/js/main.js
+++ Courses/frontend/Javascript for beginners/js/main.js
@@ -3,6 +3,7 @@ var btn = document.getElementById('go-button');
 function buttonClicked(){
     console.log("Button Clicked");
     btn.removeEventListener('click', buttonClicked);
+    document.getElementById('text').innerHTML = "Don't Click";
 }
 
 btn.addEventListener('click', buttonClicked);
