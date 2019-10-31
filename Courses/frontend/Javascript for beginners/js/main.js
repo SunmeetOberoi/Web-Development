9fae19a6d709bb79b94a55c0b4dafbc0ccb2aa0d
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-10-31 17:26:52 +0530

    Arrays

--- Courses/frontend/Javascript for beginners/js/main.js
+++ Courses/frontend/Javascript for beginners/js/main.js
@@ -6,4 +6,28 @@ function buttonClicked(){
     document.getElementById('text').innerHTML = "Don't Click";
 }
 
+var hobbies = ['Pizza', 'Dancing', 'Programming', 'Gaming', 'Music'];
+
+console.log(hobbies);
+console.log('I no longer enjoy', hobbies.pop());
+console.log(hobbies);
+hobbies.push('Archery');
+console.log(hobbies);
+console.log(hobbies.shift());
+hobbies.unshift("Laughing");
+console.log(hobbies);
+
+hobbies.forEach(function(item, index){
+    console.log("I like", item, index);
+});
+
+if(hobbies.indexOf("Gaming")>-1){
+    console.log("I like Gaming");
+    console.log(hobbies.splice(hobbies.indexOf("Gaming"), 2));
+    console.log(hobbies);
+}
+if(hobbies.indexOf("Sports")===-1){
+    console.log("Sports not found in array")
+}
+
 btn.addEventListener('click', buttonClicked);
