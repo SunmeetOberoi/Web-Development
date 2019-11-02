86bf7149a12193cc6351bfceb1bfa0e23098769e
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-11-02 17:06:03 +0530

    textAreas

--- Courses/frontend/Javascript for beginners/js/main.js
+++ Courses/frontend/Javascript for beginners/js/main.js
@@ -3,31 +3,11 @@ var btn = document.getElementById('go-button');
 function buttonClicked(){
     console.log("Button Clicked");
     btn.removeEventListener('click', buttonClicked);
-    document.getElementById('text').innerHTML = "Don't Click";
-}
-
-var hobbies = ['Pizza', 'Dancing', 'Programming', 'Gaming', 'Music'];
-
-console.log(hobbies);
-console.log('I no longer enjoy', hobbies.pop());
-console.log(hobbies);
-hobbies.push('Archery');
-console.log(hobbies);
-console.log(hobbies.shift());
-hobbies.unshift("Laughing");
-console.log(hobbies);
-
-hobbies.forEach(function(item, index){
-    console.log("I like", item, index);
-});
-
-if(hobbies.indexOf("Gaming")>-1){
-    console.log("I like Gaming");
-    console.log(hobbies.splice(hobbies.indexOf("Gaming"), 2));
-    console.log(hobbies);
-}
-if(hobbies.indexOf("Sports")===-1){
-    console.log("Sports not found in array")
+    var customText = document.getElementsByClassName("my_input");
+    var results = document.getElementById("text");
+    var textArea = document.getElementsByClassName("my_textarea");
+    results.innerHTML = "Hello, " + customText[0].value + '<br/>';
+    results.innerHTML += textArea[0].value;
 }
 
 btn.addEventListener('click', buttonClicked);
